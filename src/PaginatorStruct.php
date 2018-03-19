126bbf6f7e9eba0c56bb287093dd914d623e9278
付义兵 <websearch@163.com>
2018-03-19 15:20:25 +0800

    no message

--- src/PaginatorStruct.php
+++ src/PaginatorStruct.php
@@ -1,12 +1,9 @@
 <?php
 /**
  * 分页结构体基类，用于输出分页数据。
- *
  * 将 Phalcon 的 Paginator 结果转换成结构体。
- *
  * 定义分页结构体时，必须定义一个属性`body`，类型为数组。内容是分页输出的格式。
  */
-
 namespace Uniondrug\Structs;
 
 abstract class PaginatorStruct extends Struct
@@ -23,36 +20,33 @@ abstract class PaginatorStruct extends Struct
      */
     public static function factory($data = null)
     {
-        if (!is_object($data) || !property_exists($data, 'items') || property_exists($data, 'first')) {
-            throw new \RuntimeException('input data must be an Paginator object');
-        }
+        // phalcon QueryBuild response compatiable.
+        // if (!is_object($data) || !property_exists($data, 'items') || property_exists($data, 'first')) {
+        //    throw new \RuntimeException('input data must be an Paginator object');
+        // }
         $struct = new static();
-
         if (!$struct->has('body')) {
-            throw new \RuntimeException('Property \'body\' for \'' . get_class($struct) . '\' must be defined');
+            throw new \RuntimeException('Property \'body\' for \''.get_class($struct).'\' must be defined');
         }
         if (substr($struct->_definition['body'], -2) != '[]') {
-            throw new \RuntimeException('Property \'body\' for \'' . get_class($struct) . '\' must be defined as an array (end with [])');
+            throw new \RuntimeException('Property \'body\' for \''.get_class($struct).'\' must be defined as an array (end with [])');
         }
-
         // 分页结构
         $struct->paging = PagingStruct::factory([
-            'first'      => $data->first,
-            'before'     => $data->before,
-            'current'    => $data->current,
-            'next'       => $data->next,
-            'last'       => $data->last,
+            'first' => $data->first,
+            'before' => $data->before,
+            'current' => $data->current,
+            'next' => $data->next,
+            'last' => $data->last,
             'totalPages' => $data->total_pages,
             'totalItems' => $data->total_items,
         ]);
-
         // 数据
         $dataType = substr($struct->_definition['body'], 0, -2);
         foreach ($data->items as $item) {
             // 只获取一层数据
             $struct->body[] = $dataType::factory($item->toArray());
         }
-
         return $struct;
     }
 }
