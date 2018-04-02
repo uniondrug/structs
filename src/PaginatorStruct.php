45e6079682d9b947e61c2fb2f1578f8e1c3a7449
付义兵 <websearch@163.com>
2018-04-02 17:55:16 +0800

    结构支持，原样返回序列化 Backend中将拿到的数据销作合并后，原样转成Struct将给withStruct

--- src/PaginatorStruct.php
+++ src/PaginatorStruct.php
@@ -23,15 +23,53 @@ abstract class PaginatorStruct extends Struct
      */
     public function __construct($data, $end = true)
     {
-        // 1. 对象实例化
+        // 1. 必须结构检查
         parent::__construct(null, false);
         $this->hasListProperty();
         $this->hasPagingProperty();
-        // 2. 数据格式不合法
-        if (!property_exists($data, 'items') || !$this->isIteratorAble($data->items)) {
+        // 2. 数据源检查
+        if (is_array($data)) {
+            $this->__constructArray($data);
+        } else if (is_object($data) && $data instanceof \stdClass) {
+            $this->__constructObject($data);
+        }
+        // 3. End
+        if ($end === true) {
+            $this->endWith();
+        }
+    }
+
+    /**
+     * 数据组入参
+     * @param array $data
+     */
+    private function __constructArray(array $data)
+    {
+        // 1. 分页
+        if (isset($data[static::STRUCT_PAGING_COLUMN])) {
+            $this->with([static::STRUCT_PAGING_COLUMN => $data[static::STRUCT_PAGING_COLUMN]]);
+        } else {
+            $this->with([static::STRUCT_PAGING_COLUMN => []]);
+        }
+        // 2. 数据
+        if (isset($data[static::STRUCT_LIST_COLUMN])) {
+            $this->with([static::STRUCT_LIST_COLUMN => $data[static::STRUCT_LIST_COLUMN]]);
+        } else {
+            $this->with([static::STRUCT_LIST_COLUMN => []]);
+        }
+    }
+
+    /**
+     * 对象入参
+     * @param \stdClass $data
+     */
+    private function __constructObject(\stdClass $data)
+    {
+        // 1. 数据格式不合法
+        if (!$this->isIteratorAble($data->items)) {
             throw new Exception("用于属性'{$this->getClassName()}::\$".static::STRUCT_LIST_COLUMN."'的数据源不是可迭格式");
         }
-        // 3. 分页参数赋值
+        // 2. 分页参数赋值
         $this->with([
             static::STRUCT_PAGING_COLUMN => [
                 'first' => $data->first,
@@ -44,11 +82,7 @@ abstract class PaginatorStruct extends Struct
                 'totalItems' => $data->total_items
             ]
         ]);
-        // 4. 数据列表赋值
+        // 3. 数据列表赋值
         $this->with([static::STRUCT_LIST_COLUMN => $data->items]);
-        // 5. End
-        if ($end === true) {
-            $this->endWith();
-        }
     }
 }
